229b14dfe76b69782bccc6c99222ce340b707f63
Javier Arroyo <javier.arroyo@veeva.com>
2022-12-01 16:25:56 +0100

    TASK: BuildConventions - Prepare to use plugins

--- app/src/main/java/com/jarroyo/composeapp/MainActivity.kt
+++ app/src/main/java/com/jarroyo/composeapp/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     Greeting("Android")
                 }
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(
     name: String,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Text(text = "Hello $name!", modifier = modifier)
 }
